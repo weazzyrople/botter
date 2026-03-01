e4a319675d260344edd41df5ad4b01a90c149f98
weazzyrople <usmonxadjaevibrohm@gmail.com>
2026-03-01 05:47:07 +0500

    Update Phones collection bot.py

--- Phones collection bot.py
+++ Phones collection bot.py
@@ -391,7 +391,12 @@ async def help_menu_callback(callback: types.CallbackQuery):
     
     await callback.message.delete()
     await callback.bot.send_photo(
-        chat_id=callback.message.chat.id,
+    chat_id=callback.message.chat.id,
+    photo="https://i.postimg.cc/HsVQVsZQ/photo_2026_03_01_03_52_06.jpg"
+    caption=f"ℹ️ ...",
+    parse_mode="HTML",
+    reply_markup=keyboard
+)
         caption=f"ℹ️ <b>Наш бот представляет из себя инструмент для "
                 f"коллекционирования различных моделей телефонов: от старого "
                 f"хлама до новых ультра флагманов.</b>\n\n"
